6b9bd78559196758f85fc83866e3ab88ff0742c2
Rani Abu Sultan <raniabusultan@gmail.com>
2019-02-13 09:33:58 +0300

    Adding data to firestore

--- index.js
+++ index.js
@@ -24,4 +24,24 @@ btns.forEach(btn => {
     formAct.textContent = activity;
     
   })
+});
+
+// form submit
+form.addEventListener('submit', e => {
+  // prevent default action
+  e.preventDefault();
+
+  const distance = parseInt(input.value);
+  if(distance) {
+    db.collection('activities').add({
+      distance: distance,
+      activity: activity,
+      date: new Date().toString()
+    }).then(() => {
+      error.textContent = '';
+      input.value = '';
+    })
+  } else {
+    error.textContent = 'Please enter a valid distance';
+  }
 })
